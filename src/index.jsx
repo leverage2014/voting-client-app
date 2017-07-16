6c43c2ab01b0d2a87d633a9f550adc0ec0cde6a7
leverage2014 <myletter428@gmail.com>
2017-07-16 15:42:19 -0700

    refactor voting component and add unittests

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Voting from './componets/Voting';
-
-// const pair = ['Trainspotting', '28 days later'];
-
-// ReactDOM.render(
-// 	<Voting pair={pair}/>,
-// 	document.getElementById('app')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
@@ -16,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} hasVoted='Trainspotting'/>,
   document.getElementById('app')
 );
