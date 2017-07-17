80b101a6c29ae9c681be3dd876bda076d58a72f8
leverage2014 <myletter428@gmail.com>
2017-07-16 17:54:54 -0700

    add reducer

---
+++ test/reducer_spec.js
@@ -0,0 +1,66 @@
+import { List, Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+	it('handle SET_STATE', () => {
+		const initialState = Map();
+		const action = {
+			type: 'SET_STATE',
+			state: Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({ Trainspotting: 1 })
+				})
+			})
+		};
+		const nextState = reducer(initialState, action);
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later'],
+				tally: { Trainspotting: 1 }
+			}
+		}));
+	});
+
+	it('handle SET_STATE with plain JS payload', () => {
+		const initialState = Map();
+		const action = {
+			type: 'SET_STATE',
+			state: {
+				vote: {
+					pair: ['Trainspotting', '28 days later'],
+					tally: { Trainspotting: 1 }
+				}
+			}
+		};
+		const nextState = reducer(initialState, action);
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later'],
+				tally: { Trainspotting: 1 }
+			}
+		}));
+	});
+
+	it('handle SET_STATE without initial state', () => {
+		const action = {
+			type: 'SET_STATE',
+			state: {
+				vote: {
+					pair: ['Trainspotting', '28 days later'],
+					tally: { Trainspotting: 1 }
+				}
+			}
+		};
+		const nextState = reducer(undefined, action);
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later'],
+				tally: { Trainspotting: 1 }
+			}
+		}));
+	});
+});
